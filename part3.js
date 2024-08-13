3598991128f7dd03581799379d086371ec96a8bf
Derek B <129473867+bublysoda@users.noreply.github.com>
2024-08-13 15:51:07 -0500

    Update part3.js

--- part3.js
+++ part3.js
@@ -14,3 +14,31 @@ function applyBoard1(ID){
     while(countUp != 101){applyBoard1(countUp)}
 }
 applyBoard1(countUp)
+
+
+
+//NOW THE GAME STARTS
+export function playerAttack(){
+    let informationChange = document.querySelector('.info')
+    informationChange.textContent = 'Click an enemy space to attack it!'
+    let countUp = 1
+    function setUpBoard2(ID){
+        let button2Grab = document.getElementById('R'+ID)
+        button2Grab.addEventListener("click", function(){
+            let attackCoord = button2Grab.textContent
+            part1.receiveAttack(1, attackCoord)
+        })
+        countUp++
+        while(countUp != 101){setUpBoard2(countUp)}
+    }
+    setUpBoard2(1)
+}
+
+
+export function computerAttack(){
+    let informationChange = document.querySelector('.info')
+    informationChange.textContent = 'It is the computers turn to attack'
+    let computerChoice = Math.floor(Math.random() * 101)
+    console.log(`The computer is attacking ${computerChoice}`)
+    part1.receiveAttack(0, computerChoice)
+}
